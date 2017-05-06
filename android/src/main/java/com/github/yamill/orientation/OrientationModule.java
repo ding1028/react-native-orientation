085add0a3045eb819408a2d5cec5118512108717
Cameron Moreau <cameron.cm6@gmail.com>
2017-05-06 00:32:13 -0500

    Fixed unregister bug for android

--- android/src/main/java/com/github/yamill/orientation/OrientationModule.java
+++ android/src/main/java/com/github/yamill/orientation/OrientationModule.java
@@ -158,19 +158,10 @@ public class OrientationModule extends ReactContextBaseJavaModule implements Lif
             activity.unregisterReceiver(receiver);
         }
         catch (java.lang.IllegalArgumentException e) {
-            FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
+            FLog.e(ReactConstants.TAG, "Failed to unregister reciever", e);
         }
     }
 
     @Override
     public void onHostDestroy() {
-        final Activity activity = getCurrentActivity();
-        if (activity == null) return;
-        try
-        {
-            activity.unregisterReceiver(receiver);
-        }
-        catch (java.lang.IllegalArgumentException e) {
-            FLog.e(ReactConstants.TAG, "receiver already unregistered", e);
-        }}
     }
